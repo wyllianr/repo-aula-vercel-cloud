62803ffb310afc33be650fcf5e11ce8436a9de30
wyllianr <wyribiski@gmail.com>
2026-05-12 20:01:07 -0300

    teste professor

--- calculadora-qualidade-software/src/App.tsx
+++ calculadora-qualidade-software/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Calculadora master blaster</h1>
+      <h1>Calculadora master blaster wyllian</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           {count}
